0310dfe23a77dc410f6f23f69a6361670375a4e7
vagrant <utsavvakil@gmail.com>
2016-10-04 01:47:24 +0000

    Adding first time

--- pytree.py
+++ pytree.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 
 
-# YOUR CODE GOES here
+indent = "|   "
+indent_done = "    "
+child_branch = "|-- "
+child_branch_done = "`-- "
+count_dir = 0
+count_files = 0
+def print_tree(dir_path,offset):
+    global count_dir
+    global count_files
+    all_children = os.listdir(dir_path)
+    children = []
+    for x in all_children:
+        if x[0] != '.':
+            children.append(x)
+    children = sorted(children, key=lambda s: s.strip('_').lower())
+    i = 0
+    end_flag = 0
+    for child in children:
+        if(i < len(children) - 1):
+            print(offset + child_branch + str(child))
+        else:
+            end_flag = 1
+            print(offset + child_branch_done + str(child))
 
-
+        if(os.path.isdir(os.path.join(dir_path, child))):
+            count_dir += 1
+            if(end_flag == 1):
+                print_tree(os.path.join(dir_path, child), offset + indent_done)
+            else:
+                print_tree(os.path.join(dir_path, child), offset + indent)
+        elif(os.path.isfile(os.path.join(dir_path, child))):
+            count_files += 1
+        i += 1
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+	dir_path = "."
+	count_dir = 0
+	count_files = 0
+	if(len(sys.argv) == 2):
+		dir_path = sys.argv[1]
+	if(len(sys.argv) > 2):
+		print("Invalid data")
+		sys.exit()
+	print(dir_path)
+	print_tree(dir_path,"") 
+	print()
+	final_str = str(count_dir) + " directories, "+ str(count_files) + " files"
+	print(final_str)
+
